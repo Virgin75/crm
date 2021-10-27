561762fb9db9b44fe9fc68104ae43507e51c919b
Virgin Bitton <virgin.bitton@gmail.com>
2021-10-27 22:12:07 +0200

    ajout du modèle CLient

--- crmapp/crm/models.py
+++ crmapp/crm/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CLIENT_TYPES = [
+    ('lead', 'Lead'),
+    ('customer', 'Customer')
+]
+
+
+class Clients(models.Model):
+
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    phone = models.IntegerField()
+    mobile = models.IntegerField()
+    client_type = models.CharField(max_length=10,
+                                   choices=CLIENT_TYPES,
+                                   default='lead')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    sales_contact_id = models.ForeignKey(User, on_delete=models.CASCADE)

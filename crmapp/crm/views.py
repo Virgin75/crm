0f995fe3eea3630882d4a2141cfda323bc736ee0
Virgin Bitton <virgin.bitton@gmail.com>
2021-11-07 16:21:05 +0100

    refacto de la vue CreateClient pour aussi lister les clients de l'utilisateur

--- crmapp/crm/views.py
+++ crmapp/crm/views.py
@@ -3,16 +3,21 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
 from .models import Client
 from .serializers import ClientSerializer
 from .permissions import IsSalesUser
 
 
-class CreateClient(CreateAPIView):
+class CreateClient(ListCreateAPIView):
     permission_classes = (IsSalesUser | IsAdminUser,)
     serializer_class = ClientSerializer
+
+    def get_queryset(self):
+        '''Get only the list of clients of the user.'''
+        user = self.request.user
+        return Client.objects.filter(sales_contact=user)
 
     def perform_create(self, serializer):
         serializer.save(sales_contact=self.request.user)

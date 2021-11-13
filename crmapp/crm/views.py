83a293e866e3ecb5faf4c1c42ff1f5e0d0ece0bc
Virgin Bitton <virgin.bitton@gmail.com>
2021-11-13 10:50:25 +0100

    ajout des query params avec filtres dans le vue pour les objets Contrat

--- crmapp/crm/views.py
+++ crmapp/crm/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAdminUser
+from datetime import datetime
 from .models import Client, Contract, Event
 from .serializers import ClientSerializer, ContractSerializer, EventSerializer
 from .permissions import IsSalesUser, IsClientOwner, SalesCanCreateSupportCanList, IsSupportUser, IsEventOwner, IsOwner
@@ -22,9 +23,9 @@ class ListCreateClient(ListCreateAPIView):
         email = self.request.query_params.get('email')
 
         if name is not None:
-            queryset = queryset.filter(last_name__contains=name)
+            queryset = queryset.filter(last_name__icontains=name)
         if email is not None:
-            queryset = queryset.filter(email__contains=email)
+            queryset = queryset.filter(email__icontains=email)
 
         return queryset
 
@@ -50,8 +51,35 @@ class ListCreateContract(ListCreateAPIView):
     def get_queryset(self):
         '''Get only the list of contracts of the user's clients.'''
         user = self.request.user
+        client_name = self.request.query_params.get('client_name')
+        client_email = self.request.query_params.get('client_email')
+        amount_min = self.request.query_params.get('amount_min')
+        amount_max = self.request.query_params.get('amount_max')
+        # Expected date format in query params : 2021-12-31
+        date_min = datetime.strptime(
+            self.request.query_params.get('date_min'), '%Y-%m-%d'
+        )
+        date_max = datetime.strptime(
+            self.request.query_params.get('date_max'), '%Y-%m-%d'
+        )
+
         client_list = Client.objects.filter(sales_contact=user)
-        return Contract.objects.filter(client__in=client_list)
+        queryset = Contract.objects.filter(client__in=client_list)
+
+        if client_name is not None:
+            name_filter = Client.objects.filter(last_name__icontains=client_name)
+            queryset = queryset.filter(client__in=name_filter)
+        if client_email is not None:
+            email_filter = Client.objects.filter(email__icontains=client_email)
+            queryset = queryset.filter(client__in=email_filter)
+        if amount_min is not None:
+            queryset = queryset.filter(amount__gte=amount_min)
+        if amount_max is not None:
+            queryset = queryset.filter(amount__lte=amount_max)
+        if date_min is not None and date_max is not None:
+            queryset = queryset.filter(created_at__range=(date_min, date_max))
+
+        return queryset
 
     def perform_create(self, serializer):
         serializer.save(sales_contact=self.request.user)

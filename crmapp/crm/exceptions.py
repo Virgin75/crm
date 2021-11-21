79196488f4a0063c28551ae5e5ff3d5a750715c6
Virgin Bitton <virgin.bitton@gmail.com>
2021-11-21 18:45:22 +0100

    changement du code d'erreur 503 en 422 pour le ValueError de la date

--- crmapp/crm/exceptions.py
+++ crmapp/crm/exceptions.py
@@ -2,5 +2,5 @@ from rest_framework.exceptions import APIException
 
 
 class WrongDateFormat(APIException):
-    status_code = 503
+    status_code = 422
     default_detail = 'Wrong date format : Please follow this format: YYYY-MM-DD'
